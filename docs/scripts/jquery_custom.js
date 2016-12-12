5d5a06f4f298ba82f67866a5495d36d03c376836
jeanpaulsio <sio.jeanpaul@gmail.com>
2016-12-11 20:18:57 -0800

    basic grid

---
+++ docs/scripts/jquery_custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	// Create Game Board
+	var boxCount = 900; // number of squares
+	for(var i = 1; i <= boxCount; i++){
+	    $(".container").append('<div class="box"></div>');
+	}
+
+});
