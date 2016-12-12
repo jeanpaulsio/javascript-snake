abe298b15c42f0200592114ad410932bdbb2b58a
jeanpaulsio <sio.jeanpaul@gmail.com>
2016-12-11 23:55:18 -0800

    define edges of game board

--- docs/scripts/jquery_custom.js
+++ docs/scripts/jquery_custom.js
@@ -1,26 +1,52 @@
 $(document).ready(function(){
-	
-	// Number of Boxes on Board
-	var boxCount = 1200;
+  
+  // Number of Boxes on Board
+  var boxCount = 1200;
 
-	// Random Box Number
-	var randNum = (Math.random() * 1200) + 1;
+  // Random Box Number
+  var randNum = (Math.random() * 1200) + 1;
 
-	// Snake Body
-	var snakeBody = [612, 613, 614, 615, 616, 617];
-	if(snakeBody.indexOf(parseInt(randNum)) != -1) {
-		randNum += 10
-	}
+  // Snake Body
+  var snakeBody = [612, 613, 614, 615, 616, 617];
+  if(snakeBody.indexOf(parseInt(randNum)) != -1) {
+    randNum += 10
+  }
 
-	// Create Game Board
-	for(var i = 1; i <= boxCount; i++) {
-	  if(i == parseInt(randNum)){
-	  	$(".container").append('<div class="box apple"></div>');
-	  } else if(snakeBody.indexOf(i) !== -1) {
-	  	$(".container").append('<div class="box snake-body"></div>');
-	  } else {
-	  	$(".container").append('<div class="box"></div>');
-	  }
-	}
+  // Edges
+  var topEdgeMin = 1;
+  var topEdgeMax = 30;
+  var topEdgeArr = [];
+  while(topEdgeMin <= topEdgeMax){ topEdgeArr.push(topEdgeMin++); }
+
+  var botEdgeMin = 1171;
+  var botEdgeMax = 1200;
+  var botEdgeArr = [];
+  while(botEdgeMin <= botEdgeMax){ botEdgeArr.push(botEdgeMin++); }
+
+  var leftEdgeMin = 1;
+  var leftEdgeMax = 1200;
+  var leftEdgeArr = [];
+  while(leftEdgeMin <= leftEdgeMax){ leftEdgeArr.push(leftEdgeMin+=30); }
+
+  var rightEdgeArr = leftEdgeArr.map(function(i){return i - 1});
+  
+  // Create Game Board
+  for(var i = 1; i <= boxCount; i++) {
+    if(topEdgeArr.indexOf(i) !== -1) {
+      $(".container").append('<div class="box edge"></div>');
+    } else if(botEdgeArr.indexOf(i) !== -1) {
+      $(".container").append('<div class="box edge"></div>');
+    } else if(leftEdgeArr.indexOf(i) !== -1) {
+      $(".container").append('<div class="box edge"></div>');
+    } else if(rightEdgeArr.indexOf(i) !== -1) {
+      $(".container").append('<div class="box edge"></div>');
+    } else if(i == parseInt(randNum)){
+      $(".container").append('<div class="box apple"></div>');
+    } else if(snakeBody.indexOf(i) !== -1) {
+      $(".container").append('<div class="box snake-body"></div>');
+    } else {
+      $(".container").append('<div class="box"></div>');
+    }
+  }
 
 });
