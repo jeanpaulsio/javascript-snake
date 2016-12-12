d8d182dc3cd5c1835488f98c8728df1e203a2097
jeanpaulsio <sio.jeanpaul@gmail.com>
2016-12-11 22:25:02 -0800

    finish styling

--- docs/scripts/jquery_custom.js
+++ docs/scripts/jquery_custom.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
 	
+	// Number of Boxes on Board
+	var boxCount = 1200;
+
+	// Random Box Number
+	var randNum = (Math.random() * 1200) + 1;
+
+	// Snake Body
+	var snakeBody = [613, 614, 615, 616];
+	if(snakeBody.indexOf(parseInt(randNum)) != -1) {
+		randNum += 10
+	}
+
 	// Create Game Board
-	var boxCount = 1200; // number of squares
-	for(var i = 1; i <= boxCount; i++){
-	    $(".container").append('<div class="box"></div>');
+	for(var i = 1; i <= boxCount; i++) {
+	  if(i == parseInt(randNum)){
+	  	$(".container").append('<div class="box apple"></div>');
+	  } else if(snakeBody.indexOf(i) !== -1) {
+	  	$(".container").append('<div class="box snake-body"></div>');
+	  } else {
+	  	$(".container").append('<div class="box"></div>');
+	  }
 	}
 
 });
