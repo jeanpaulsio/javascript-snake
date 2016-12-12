084fbdc90bd9dbe514104cac9ee8d39e9a1072d1
jeanpaulsio <sio.jeanpaul@gmail.com>
2016-12-12 15:09:45 -0800

    change how grid is generated

--- docs/scripts/jquery_custom.js
+++ docs/scripts/jquery_custom.js
@@ -1,53 +1,26 @@
 $(document).ready(function(){
-  
-  // Number of Boxes on Board
-  var boxCount = 1200;
+  let gameBoard = [];
 
-  // Random Box Number
-  var randNum = (Math.random() * 1200) + 1;
+  for(var i=15; i >= -15; i--){
+      let rowMin = -15;
+      let rowMax = 15;
+      let row    = [];
+      while(rowMin <= rowMax){ row.push([i, rowMin++]); };
+    
+    gameBoard.push(row);
+  };
 
-  // Snake Body
-  var snakeBody = [612, 613, 614, 615, 616, 617];
-  if(snakeBody.indexOf(parseInt(randNum)) != -1) {
-    randNum += 10
-  }
-
-  // Edges
-  var topEdgeMin = 1;
-  var topEdgeMax = 30;
-  var topEdgeArr = [];
-  while(topEdgeMin <= topEdgeMax){ topEdgeArr.push(topEdgeMin++); }
-
-  var botEdgeMin = 1171;
-  var botEdgeMax = 1200;
-  var botEdgeArr = [];
-  while(botEdgeMin <= botEdgeMax){ botEdgeArr.push(botEdgeMin++); }
-
-  var leftEdgeMin = 1;
-  var leftEdgeMax = 1200;
-  var leftEdgeArr = [];
-  while(leftEdgeMin <= leftEdgeMax){ leftEdgeArr.push(leftEdgeMin+=30); }
-
-  var rightEdgeArr = leftEdgeArr.map(function(i){return i - 1});
-  
-  // Create Game Board
-  for(var i = 1; i <= boxCount; i++) {
-    if(topEdgeArr.indexOf(i) !== -1) {
-      $(".container").append('<div class="box edge"></div>');
-    } else if(botEdgeArr.indexOf(i) !== -1) {
-      $(".container").append('<div class="box edge"></div>');
-    } else if(leftEdgeArr.indexOf(i) !== -1) {
-      $(".container").append('<div class="box edge"></div>');
-    } else if(rightEdgeArr.indexOf(i) !== -1) {
-      $(".container").append('<div class="box edge"></div>');
-    } else if(i == parseInt(randNum)){
-      $(".container").append('<div class="box apple"></div>');
-    } else if(snakeBody.indexOf(i) !== -1) {
-      $(".container").append('<div class="box snake-body"></div>');
-    } else {
-      $(".container").append('<div class="box"></div>');
+  for (let i of gameBoard) {
+    for(let j of i){
+      $(".container").append('<div class="box" name="['+ j +']"></div>');
     }
-  }
+  };
+
+  $("div[name*='[0,-2]']").addClass("snake-body");
+  $("div[name*='[0,-1]']").addClass("snake-body");
+  $("div[name*='[0,0]']").addClass("snake-body");
+  $("div[name*='[0,1]']").addClass("snake-body");
+  $("div[name*='[0,2]']").addClass("snake-body");
 });
 
 $(document).keydown(function(e){
