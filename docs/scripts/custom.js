dffb2e29f023ccf17566c6f0b359c231a81aae6c
jeanpaulsio <sio.jeanpaul@gmail.com>
2017-02-22 10:27:27 -0800

    implements basic snake logic

--- docs/scripts/custom.js
+++ docs/scripts/custom.js
@@ -1,6 +1,8 @@
-let snake = [0, 1, 2];
-let currentPosition = "right";
-let gameSpeed = 333;
+let snake            = [25,26,27];
+let currentDirection = "right";
+let gameSpeed        = 300;
+let endGameStatus    = false
+let fruitCoord       = 29;
 
 function getTarget(snake, direction) {
   let snakeHead = snake[snake.length - 1]
@@ -8,77 +10,114 @@ function getTarget(snake, direction) {
   switch(direction) {
     case "right": return snakeHead + 1;
     case "left":  return snakeHead - 1;
-    case "up":    return snakeHead - 10;
-    case "down":  return snakeHead + 10;
+    case "up":    return snakeHead - 11;
+    case "down":  return snakeHead + 11;
   }
 }
 
 function setPosition(position) {
-  currentPosition = position
+  currentDirection = position
 }
 
-function move(snake, position=currentPosition) {
+function moveSnake(snake, position=currentDirection) {
   window.addEventListener('keydown', function (e){
-    if (e.keyCode == 37) {
-      setPosition("left")
-    } else if (e.keyCode == 38) {
-      setPosition("up")
-    } else if (e.keyCode == 39) {
-      setPosition("right")
-    } else if(e.keyCode == 40){
-      setPosition("down")
+    switch(e.keyCode) {
+      case 37: return setPosition("left");
+      case 38: return setPosition("up");
+      case 39: return setPosition("right");
+      case 40: return setPosition("down");
     }
   });
 
-  console.log(position);
-
-  let target = getTarget(snake, position)
+  let target = getTarget(snake, position);
 
   for (let i = 0; i < snake.length; i++) {
-    snake[i] = snake[i+1]
+    snake[i] = snake[i+1];
   }
 
   snake[snake.length - 1] = target;
-  console.log(snake);
+
+  console.log(snake)
+}
+
+function feedSnake(snake) {
+  switch(snake[0] - snake[1]) {
+    case -1:
+      snake.unshift(snake[0] - 1);
+      break;
+    case 1:
+      snake.unshift(snake[0] + 1);
+      break;
+    case -11:
+      snake.unshift(snake[0] -11);
+      break;
+    case 11:
+      snake.unshift(snake[0] + 11);
+      break;
+  }
+  placeFruit();
+}
+
+function placeFruit() {
+  let randNumber = Math.floor((Math.random() * 130) + 1);
+  fruitCoord = randNumber
 }
 
 function endGame(){
-  false;
+  return endGameStatus;
+}
+
+function toggleGameStatus(){
+  endGameStatus = endGameStatus ? false : true;
+}
+
+function resumeGame(){
+  endGameStatus = false;
+  animateBoard();
 }
 
 function drawSnake(board){
   for(let i = 0; i < board.length; i++) {
     if (snake.includes(i)) {
-      board[i] = ((i + 1) % 10 == 0) ? "|_X_|<br>" : "|_X_|";
+      if (board[i].includes("O")) { toggleGameStatus() }
+      board[i] = ((i + 1) % 11 == 0) ? "|_X_|<br>" : "|_X_|";
     }
   }
 }
 
 function drawBoard() {
   let board = [
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|",
-  "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|"
+  "O", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "|___|", "O",
+  "O", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "|_O_|", "O"
   ]
+  board[fruitCoord] = "|_@_|"
 
   for (let i = 0; i < board.length; i++) {
-    board[i] = ((i + 1) % 10 == 0) ? "|___|<br>" : `${board[i]}`
+    board[i] = ((i + 1) % 11 == 0) ? `${board[i] += '<br>'}` : `${board[i]}`
+  }
+
+  if(snake[snake.length-2] == fruitCoord){
+    feedSnake(snake)
   }
 
   drawSnake(board);
-  move(snake)
+  moveSnake(snake)
   document.getElementById('board').innerHTML = board.join('');
 }
 
 function animateBoard(){
-  setTimeout(()=> {
+  setTimeout( () => {
     drawBoard();
     if (!endGame()) { animateBoard() }
   }, gameSpeed)
